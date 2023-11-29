9e002ad39fcfa06bf516ce27d5191b76d643ba8e
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-11-29 18:55:43 +0330

    jalase 11 Product Type - Product Brand

---
+++ Src/Application/Features/ProductsBrand/Queries/GetAll/GetAllProductBrandQuery.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.ProductEntity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductsBrand.Queries.GetAll
+{
+    public class GetAllProductBrandQuery : IRequest<IEnumerable<ProductBrand>>
+    {
+    }
+}
---
+++ Src/Application/Features/ProductsBrand/Queries/GetAll/ProductBrandSpec.cs
@@ -0,0 +1,18 @@
+﻿using Application.Contracts.Specification;
+using Domain.Entities.ProductEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductsBrand.Queries.GetAll
+{
+    public class ProductBrandSpec : BaseSpecification<ProductBrand>
+    {
+        public ProductBrandSpec()
+        {
+
+        }
+    }
+}
