f456940d1a389cda1baea257864620baca6a3302
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-11-29 12:11:16 +0330

    jalase 3 Add Products With Context

---
+++ Src/Web/ConfigureService.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web
+{
+    public static class ConfigureService
+    {
+        public static IServiceCollection AddWebServiceCollection(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            return builder.Services;
+        }
+    }
+}
